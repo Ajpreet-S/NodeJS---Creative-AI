c83dc18b5d7555a35611c17dae918ce4dc884426
Ajpreet Singh <SINGHAG19@juniata.edu>
2023-04-06 20:17:26 -0400

    changed the entry point from index.js to app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
 'use strict';
+
 
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send('Initial commit');
 })
+
 
 app.listen(process.env.PORT || 3000);
